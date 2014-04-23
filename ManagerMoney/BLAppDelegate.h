746c57ca8ff77e805519ed30e46b9db963d21d92
BRUCE LAU <brucelau@BRUCEtekiMacBook-Air.local>
2014-04-23 09:11:07 +0800

    Initial Commit

---
+++ ManagerMoney/BLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLAppDelegate.h
+//  ManagerMoney
+//
+//  Created by BRUCE LAU on 14-4-23.
+//  Copyright (c) 2014年 BRUCE LAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ManagerMoney/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ManagerMoney
+//
+//  Created by BRUCE LAU on 14-4-23.
+//  Copyright (c) 2014年 BRUCE LAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLAppDelegate class]));
+    }
+}
